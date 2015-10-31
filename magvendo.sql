9d422740cefde07a3f57c3eff35bcca6a89f6dc4
Iurie Nistor <iurie@sv-ti.com>
2015-10-31 14:02:38 +0200

    update database prefix

--- magvendo.sql
+++ magvendo.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.8.2
+-- version 4.2.10
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 08, 2014 at 04:18 PM
--- Server version: 5.5.31-log
--- PHP Version: 5.3.27
+-- Generation Time: Feb 19, 2015 at 07:13 PM
+-- Server version: 5.5.41-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -16,6 +16,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `mag`
+--
+
 -- --------------------------------------------------------
 
 --
@@ -23,11 +27,27 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `magv_categories` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=71 ;
+  `description` text NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=73 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `magv_customers`
+--
+
+CREATE TABLE IF NOT EXISTS `magv_customers` (
+`id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(60) DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `card` int(11) DEFAULT NULL,
+  `card_discount` tinyint(4) NOT NULL DEFAULT '0',
+  `registered_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `birthday` date DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -36,15 +56,15 @@ CREATE TABLE IF NOT EXISTS `magv_categories` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_fabricated` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
   `quantity` bigint(20) NOT NULL DEFAULT '1',
   `salary` float(20,2) NOT NULL DEFAULT '0.00',
   `date` datetime DEFAULT '0000-00-00 00:00:00',
   `magazine_id` bigint(20) DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=44 ;
+  `price` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -53,11 +73,10 @@ CREATE TABLE IF NOT EXISTS `magv_fabricated` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_magazines` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `description` text NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -66,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `magv_magazines` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_products` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
   `image` varchar(255) NOT NULL DEFAULT '',
   `description` text NOT NULL,
@@ -75,9 +94,8 @@ CREATE TABLE IF NOT EXISTS `magv_products` (
   `discount` int(3) NOT NULL DEFAULT '0',
   `category_id` bigint(10) NOT NULL,
   `archived` tinyint(1) DEFAULT '0',
-  `registered_date` datetime DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1511 ;
+  `registered_date` datetime DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM AUTO_INCREMENT=1511 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -86,13 +104,12 @@ CREATE TABLE IF NOT EXISTS `magv_products` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_products_percents` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `product_id` bigint(11) DEFAULT NULL,
   `sell_percent` float(10,2) DEFAULT NULL,
   `repare_percent` float(10,2) DEFAULT NULL,
-  `fabricated_percent` float(3,2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+  `fabricated_percent` float(3,2) DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -101,16 +118,15 @@ CREATE TABLE IF NOT EXISTS `magv_products_percents` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_repared` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
   `price` bigint(20) NOT NULL DEFAULT '0',
   `quantity` bigint(20) NOT NULL DEFAULT '1',
   `salary_percent` bigint(20) NOT NULL DEFAULT '0',
   `date` datetime DEFAULT '0000-00-00 00:00:00',
   `magazine_id` bigint(20) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=113 ;
+  `user_id` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=114 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -119,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `magv_repared` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_sales` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
   `price` bigint(20) NOT NULL DEFAULT '0',
   `quantity` bigint(20) NOT NULL DEFAULT '1',
@@ -129,8 +145,8 @@ CREATE TABLE IF NOT EXISTS `magv_sales` (
   `user_id` bigint(20) DEFAULT NULL,
   `salary_percent` bigint(20) NOT NULL DEFAULT '0',
   `product_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2838 ;
+  `card_number` int(11) DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=2854 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -139,11 +155,10 @@ CREATE TABLE IF NOT EXISTS `magv_sales` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_settings` (
-  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `value` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+`id` bigint(10) unsigned NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -152,7 +167,7 @@ CREATE TABLE IF NOT EXISTS `magv_settings` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `username` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(60) NOT NULL DEFAULT '',
   `email` varchar(60) NOT NULL DEFAULT '',
@@ -164,9 +179,8 @@ CREATE TABLE IF NOT EXISTS `magv_users` (
   `status` tinyint(1) DEFAULT '0',
   `role` tinyint(1) DEFAULT '0',
   `registered_date` datetime DEFAULT '0000-00-00 00:00:00',
-  `last_login` datetime DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
+  `last_login` datetime DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -175,13 +189,141 @@ CREATE TABLE IF NOT EXISTS `magv_users` (
 --
 
 CREATE TABLE IF NOT EXISTS `magv_workers` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`id` bigint(20) unsigned NOT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   `work_role` int(3) DEFAULT NULL,
-  `magazine_id` int(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=26 ;
+  `magazine_id` int(10) DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `magv_categories`
+--
+ALTER TABLE `magv_categories`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `magv_customers`
+--
+ALTER TABLE `magv_customers`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `card` (`card`);
+
+--
+-- Indexes for table `magv_fabricated`
+--
+ALTER TABLE `magv_fabricated`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `magv_magazines`
+--
+ALTER TABLE `magv_magazines`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `magv_products`
+--
+ALTER TABLE `magv_products`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `magv_products_percents`
+--
+ALTER TABLE `magv_products_percents`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `magv_repared`
+--
+ALTER TABLE `magv_repared`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `magv_sales`
+--
+ALTER TABLE `magv_sales`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `magv_settings`
+--
+ALTER TABLE `magv_settings`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `magv_users`
+--
+ALTER TABLE `magv_users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `magv_workers`
+--
+ALTER TABLE `magv_workers`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `magv_categories`
+--
+ALTER TABLE `magv_categories`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=73;
+--
+-- AUTO_INCREMENT for table `magv_customers`
+--
+ALTER TABLE `magv_customers`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `magv_fabricated`
+--
+ALTER TABLE `magv_fabricated`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=47;
+--
+-- AUTO_INCREMENT for table `magv_magazines`
+--
+ALTER TABLE `magv_magazines`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `magv_products`
+--
+ALTER TABLE `magv_products`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1511;
+--
+-- AUTO_INCREMENT for table `magv_products_percents`
+--
+ALTER TABLE `magv_products_percents`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `magv_repared`
+--
+ALTER TABLE `magv_repared`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=114;
+--
+-- AUTO_INCREMENT for table `magv_sales`
+--
+ALTER TABLE `magv_sales`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2854;
+--
+-- AUTO_INCREMENT for table `magv_settings`
+--
+ALTER TABLE `magv_settings`
+MODIFY `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `magv_users`
+--
+ALTER TABLE `magv_users`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
+--
+-- AUTO_INCREMENT for table `magv_workers`
+--
+ALTER TABLE `magv_workers`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
